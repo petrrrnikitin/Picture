1bec35804e8e5f420db8b7af67bbd2fcb59ad0ea
Petr Nikitin <nikitablvck@gmai.com>
2020-09-22 23:04:55 +0300

    section 5 - form validation.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
 import forms from "./modules/forms";
+import mask from "./modules/maks";
+import checkTextInputs from "./modules/checkTextInputs";
+
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -8,4 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
     forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 })
