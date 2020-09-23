8a425f2d102691eff0f14afac78c3f25cb25e677
Petr Nikitin <nikitablvck@gmai.com>
2020-09-23 21:40:32 +0300

    section 6 - simple loading additional elements.

---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,18 @@
+const showMoreStyles = (trigger, styles) => {
+    const cards = document.querySelectorAll(styles),
+        button = document.querySelector(trigger);
+
+    cards.forEach(card => {
+       card.classList.add('animated', 'fadeInUp');
+    });
+
+    button.addEventListener('click', () => {
+        cards.forEach(card => {
+            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+            card.classList.add('col-sm-3', 'col-sm-offset-0' , 'col-xs-10', 'col-xs-offset-1');
+        });
+        button.style.display = 'none';
+    });
+};
+
+export default showMoreStyles;
