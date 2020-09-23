b8ad4ba79f5f2b6fc7f468d8f37643e0e3f8c8de
Petr Nikitin <nikitablvck@gmai.com>
2020-09-24 00:02:22 +0300

    section 6 - ajax loading additional elements from server.

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,18 +1,39 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-        button = document.querySelector(trigger);
+import {getResource} from "../services/requests";
 
-    cards.forEach(card => {
-       card.classList.add('animated', 'fadeInUp');
+const showMoreStyles = (trigger, wrapper) => {
+      const  button = document.querySelector(trigger);
+
+
+    button.addEventListener('click', function() {
+        getResource('http://localhost:3000/styles')
+            .then(res => {
+                console.log(res);
+                createCards(res);
+            })
+            .catch(error => console.log(error));
+
+        this.remove();
     });
 
-    button.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0' , 'col-xs-10', 'col-xs-offset-1');
+    function createCards(response) {
+        console.log(typeof response);
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML = `
+                <div class="styles-block">
+                    <img src=${src} alt="style">
+                    <h4>${title}</h4>
+                    <a href=${link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
         });
-        button.style.display = 'none';
-    });
+    }
+
 };
 
 export default showMoreStyles;
